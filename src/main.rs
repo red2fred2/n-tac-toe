dede31c9e16c70114ff5154d545f32aa4146a0cf
red2fred2 <red2fred2base@live.com>
2022-03-01 22:57:37 -0600

    Add basic blank board and display function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+/// How big of a board to make. Makes an NxN tic tac toe board
+/// Does weird stuff when N = 0
+const N: usize = 3;
+
+/// A board is a row major array of characters
+type Board = [[char; N]; N];
+
+/// Displays a board in the terminal
+///
+/// * `board` - the board to display
+fn display_board(board: Board) {
+	// Row by row
+	for i in 0..N {
+		// Print the dividing line
+		if i != 0 {
+			let line = "-".repeat(N*4-2);
+			println!("{line}");
+		}
+
+		// Print left column character
+		let c = board[i][0];
+		print!("{c}");
+
+		// Run through the rest of the row
+		for j in 1..N {
+			let c = board[i][j];
+			print!(" | {c}");
+		}
+		println!();
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	// Set blank board
+	let mut board = [[' '; N]; N];
+	board[1][2] = 'X';
+	display_board(board);
 }
